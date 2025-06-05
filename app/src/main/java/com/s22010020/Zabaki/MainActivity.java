315729822904bc3a565dc59218f374f770c417f0
Nasmiahmd <ahmednasmi2002@gmail.com>
2025-06-05 10:37:19 +0530

    First Commit

---
+++ app/src/main/java/com/s22010020/Zabaki/MainActivity.java
@@ -0,0 +1,52 @@
+package com.s22010020.Zabaki;
+
+import static com.s22010020.Zabaki.R.id.btnBack;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.home_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ImageView logoButton = findViewById(R.id.logoButton);
+        ImageView instructions = findViewById(R.id.instructions);
+
+        instructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Instruction Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // Set click listener for the circular logo
+        logoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Logo Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
